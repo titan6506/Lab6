e9e9f35e72105c5c06e740f82e6d3c65632875f7
CRISTIAM SALAZAR BARQUERO <cristiam.salazar.barquero@est.una.ac.cr>
2020-10-09 18:46:51 -0500

    Cambio a Array List

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -1,16 +1,20 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include"../Lab6/ArrayList.h"
+#include"../Lab6/IList.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace Tests
 {
-	TEST_CLASS(Tests)
+	
+	TEST_CLASS(TestsArrayList)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+	
+		TEST_METHOD(AgregarElementoALArray)
 		{
+			//ArrayList<int>* v1 = new ArrayList<int>;
 		}
 	};
 }
